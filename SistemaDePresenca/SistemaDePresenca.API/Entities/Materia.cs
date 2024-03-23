dc9842338394ad8031e84531b7bb45de46e93cb4
Bruno <bruno.edualves4@gmail.com>
2024-03-23 20:31:57 -0300

    Cria o Sistema de Presença

---
+++ SistemaDePresenca/SistemaDePresenca.API/Entities/Materia.cs
@@ -0,0 +1,34 @@
+﻿namespace SistemaDePresenca.API.Entities
+{
+    public class Materia
+    {
+        public Materia()
+        {
+            EstaAtiva = true;
+        }
+        public Guid Id { get; set; }
+        public string Nome { get; set; } 
+        public int FrequenciaExigida { get; set; }
+        public int NumeroDeFaltas { get; set; } // Numero das faltas até o dia de hoje.
+        public int TotalDeAulas { get; set; }
+        public bool EstaAtiva { get; set; }
+
+        public void Update(string nome,
+                           int frequenciaExigida,
+                           int numeroDeFaltas,
+                           int totalDeAulas,
+                           bool estaAtiva)
+        {
+            this.Nome = nome;
+            this.FrequenciaExigida = frequenciaExigida;
+            this.NumeroDeFaltas = numeroDeFaltas;
+            this.TotalDeAulas = totalDeAulas;
+            this.EstaAtiva = estaAtiva;
+        }
+
+        public void Delete()
+        {
+            this.EstaAtiva = false;
+        }
+    }
+}
---
+++ SistemaDePresenca/SistemaDePresenca.API/Persistence/SistemaDePresencaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using SistemaDePresenca.API.Entities;
+
+namespace SistemaDePresenca.API.Persistence
+{
+    public class SistemaDePresencaDbContext
+    {
+        public List<Materia> Materias { get; set; }
+
+        public SistemaDePresencaDbContext()
+        {
+            Materias = new List<Materia>();
+        }
+    }
+}
